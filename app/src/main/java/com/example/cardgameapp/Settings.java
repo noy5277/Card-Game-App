b822d7675b02370375a8e6a58b1ba532c224d691
Noy Zozel <noy5277@gmail.com>
2021-09-13 21:44:01 +0300

    add settings activity

--- app/src/main/java/com/example/cardgameapp/Settings.java
+++ app/src/main/java/com/example/cardgameapp/Settings.java
@@ -1,8 +1,10 @@
 package com.example.cardgameapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.PopupMenu;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+    }
+
+    public void PopupMenu(View view)
+    {
+        PopupMenu popup= new PopupMenu(this, view);
+        popup.getMenuInflater().inflate(R.menu.language_menu,popup.getMenu());
+        popup.setOnMenuItemClickListener(menuItem ->
+            {
+                if(menuItem.getItemId()==R.id.english)
+                {
+
+                }
+                if(menuItem.getItemId()==R.id.hebrew)
+                {
+
+                }
+                return true;
+            }
+
+        );
     }
 }
