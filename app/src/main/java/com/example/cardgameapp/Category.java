a6fb44da7f0895d71f99434a4641dc4b2a537e4d
meshiv3010 <meshiv3010@gmail.com>
2021-09-24 14:43:56 +0300

    Different and Timer

--- app/src/main/java/com/example/cardgameapp/Category.java
+++ app/src/main/java/com/example/cardgameapp/Category.java
@@ -9,12 +9,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Category extends AppCompatActivity {
     private Button backMainGame;
+    private Button OpenDifferentButton;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        OpenDifferentButton = (Button) findViewById(R.id.different);
+        OpenDifferentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenDifferent();
+            }
+        });
 
         backMainGame = (Button) findViewById(R.id.back);
         backMainGame.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +35,10 @@ public class Category extends AppCompatActivity {
         });
     }
 
+    public void OpenDifferent() {
+        Intent intent = new Intent(this, DifferentGame.class);
+        startActivity(intent);
+    }
     public void OpenMainGame() {
         Intent intent = new Intent(this,MainGame.class);
         startActivity(intent);
---
+++ app/src/main/java/com/example/cardgameapp/DifferentGame.java
@@ -0,0 +1,64 @@
+package com.example.cardgameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.service.autofill.TextValueSanitizer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class DifferentGame extends AppCompatActivity {
+
+    private Button backCategory;
+    private TextView timer;
+
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_different_game);
+
+        timer=findViewById(R.id.timerActivity);
+        long duration= TimeUnit.MINUTES.toMillis(1);
+
+        new CountDownTimer(duration, 1000) {
+            @Override
+            public void onTick(long l) {
+                String sDuration = String.format(Locale.ENGLISH, "%02d : %02d", TimeUnit.MILLISECONDS.toMinutes(l),
+                        TimeUnit.MILLISECONDS.toSeconds(l) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(l)));
+
+                timer.setText(sDuration);
+            }
+
+            @Override
+            public void onFinish() {
+
+                //when finish, hide text view
+                timer.setVisibility(View.GONE);
+
+                // Display toast
+                Toast.makeText(getApplicationContext(), "Countdown timer has ended", Toast.LENGTH_LONG).show();
+            }
+        }.start();
+
+        backCategory = (Button) findViewById(R.id.back_category);
+        backCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               OpenCategory ();
+            }
+        });
+    }
+    public void OpenCategory() {
+        Intent intent = new Intent(this,Category.class);
+        startActivity(intent);
+    }
+
+}
